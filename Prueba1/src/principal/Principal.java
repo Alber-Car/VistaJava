cd80dd0ad0c3b31b2e25dee3551704f2768c99b8
ACC <usu2@example.com>
2021-04-16 12:04:58 +0200

    Segundo mensaje

--- Prueba1/src/principal/Principal.java
+++ Prueba1/src/principal/Principal.java
@@ -15,6 +15,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo || Proyecto Git-Java");
+		System.out.println("Segundo mensaje");
 	}
 
 }
