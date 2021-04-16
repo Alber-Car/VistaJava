fbeb77e679d6d8e0e8526444a1825f8cede2195a
ACC <alber.carcar@educacyl.es>
2021-04-16 12:03:41 +0200

    Primera validacion de principal

---
+++ Prueba1/src/principal/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package principal;
+
+/**
+ * @author usuario
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo || Proyecto Git-Java");
+	}
+
+}
