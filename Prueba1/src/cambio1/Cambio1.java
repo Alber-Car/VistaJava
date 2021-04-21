f0e91f2c752c920f5da0874560f43ac69c3a2068
ACC <usu2@example.com>
2021-04-21 13:42:48 +0200

    Validacion de la clase cambio1 en cambio1

---
+++ Prueba1/src/cambio1/Cambio1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cambio1;
+
+/**
+ * @author usuario
+ *
+ */
+public class Cambio1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola en cambio1");
+		System.out.println("Hey de cambio1");
+	}
+
+}
